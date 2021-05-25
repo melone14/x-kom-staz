ddd8761914a4456df325ff3e85737d0e73dbf23a
meloniak14 <w.melone97@gmail.com>
2021-05-25 22:37:23 +0100

    Add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
-import { Counter } from "./features/counter/Counter";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { Counter } from "./features/counter/Counter";
+import { Home } from "./pages/Home";
+import { Reservation } from "./pages/Reservation";
+import { Summary } from "./pages/Summary";
 
 function App() {
-  return <Counter />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/summary">
+          <Summary />
+        </Route>
+        <Route path="/reservation">
+          <Reservation />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
