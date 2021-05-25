f6fa194210bd964010170218a6a3a268acb26c89
meloniak14 <w.melone97@gmail.com>
2021-05-25 23:03:01 +0100

    Add ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme/theme";
 // import { Counter } from "./features/counter/Counter";
 import { Home } from "./pages/Home";
 import { Reservation } from "./pages/Reservation";
@@ -7,19 +9,21 @@ import { Summary } from "./pages/Summary";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/summary">
-          <Summary />
-        </Route>
-        <Route path="/reservation">
-          <Reservation />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/summary">
+            <Summary />
+          </Route>
+          <Route path="/reservation">
+            <Reservation />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
